fe3b12de4b574de84afb8373f8ba1e32c4a2d4d9
Aldo Grabić <aldo.grabic99@gmail.com>
2024-06-18 13:33:48 +0200

    First commit

---
+++ src/main/java/com/project/AlgoLMS/AlgoLmsApplication.java
@@ -0,0 +1,13 @@
+package com.project.AlgoLMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoLmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/AlgoLMS/AlgoLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.AlgoLMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
