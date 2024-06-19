954d00c423f7b15cefa4769e78815ee2425534d3
Aldo Grabić <aldo.grabic99@gmail.com>
2024-06-19 15:01:21 +0200

    Napravljena login stranica izgled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/main/resources/templates/**/*.html"
-  ],
+  content: ["./src/main/resources/templates/**/*.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
